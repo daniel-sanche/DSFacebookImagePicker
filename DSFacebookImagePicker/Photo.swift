4615902b1ea2111b877a6d1520c933ab4950002a
Daniel Sanche <d.sanche14@gmail.com>
2014-10-14 22:58:35 -0600

    set up photo retrieval methods

---
+++ DSFacebookImagePicker/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  DSFacebookImagePicker
+//
+//  Created by Home on 2014-10-14.
+//  Copyright (c) 2014 Sanche. All rights reserved.
+//
+
+import UIKit
+
+class Photo: NSObject {
+   
+}
