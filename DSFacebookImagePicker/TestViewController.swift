04a58f534280080f6c29b702d19bc6da2eed8f8e
Daniel Sanche <d.sanche14@gmail.com>
2014-10-16 19:20:09 -0600

    created a new storyboard  to call the Facebook picker from

---
+++ DSFacebookImagePicker/TestViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TestViewController.swift
+//  DSFacebookImagePicker
+//
+//  Created by Home on 2014-10-16.
+//  Copyright (c) 2014 Sanche. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+  @IBAction func launchPhotoPicker(sender: AnyObject) {
+  }
+
+}
